f3b227fb47be8977abdd7a9b5aba23ec27649a0f
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-08-20 20:40:28 +0530

    Cors fixed

--- server.js
+++ server.js
@@ -1,54 +1,74 @@
 import express from 'express';
-const app = express();
 import http from 'http';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import cors from 'cors';
 import connectDb from './data/database1.js';
 import { config } from 'dotenv';
-const server = http.createServer(app);
-import UserRouter from './Router/user.js';
+import UserRouter from './Router/user.js'
 import cookieParser from 'cookie-parser';
-//MiddleWare
-app.use(cors());
+
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000', // Change this to your client's origin
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    credentials: true,
+  },
+});
+app.use(cors({
+    origin: 'http://localhost:3000', // Set the allowed origin
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  }));
+;
+
+
+// Middlewares
 app.use(cookieParser());
 app.use(express.json());
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST","DELETE","PUT"],
-        credentials: true,
-    }
+
+// Routes
+app.use('/api/v1.1/users', UserRouter);
+
+// Socket.io
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+  
+  socket.on('send_message', (data) => {
+    // Emit the message to the specific room
+    const messageData = {
+      message: data.message,
+      sender: true,
+    };
+    io.to(data.room).emit('r-m', messageData);
+  });
+
+  socket.on('leave_room', (room) => {
+    socket.leave(room);
+  });
+
+  socket.on('join_room', (room) => {
+    socket.join(room);
+  });
 });
 
-const Router = express.Router();
-app.use('/api/v1.1/users',UserRouter);
-
-io.on("connection", (socket) => {
-    console.log(`User connected ${socket.id}`);
-    
-    socket.on("send_message", (data) => {
-        // Emit the message to the specific room
-        const messageData = {
-            message : data.message,
-            sender :  true,
-        }
-        io.to(data.room).emit("r-m", messageData);
-    });
-    socket.on('leave_room',(room)=>{
-        socket.leave(room);
-    })
-    socket.on('join_room', (room) => {
-        socket.join(room);
-    });
-});
-
+// Configuration
 config({
-    path: "./data/config.env",
-})
-// app.get('/',(req,res)=>{
-        // res.send('<h2>ROHAN CHAT .io</h2>');
-// })
-server.listen(process.env.PORT, () => {
-    console.log(`Listening to ${process.env.PORT}`);
+  path: './data/config.env',
 });
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('<h2>ROHAN CHAT .io</h2>');
+});
+
+// Start the server
+server.listen(process.env.PORT, () => {
+  console.log(`Listening to ${process.env.PORT}`);
+});
+
+// Connect to the database
 connectDb();
