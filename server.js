f710b20d1f147dc8e74f86a704e08c0a9eca5d3e
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-09-04 23:45:43 +0530

    Added New Domain of FireBase

--- server.js
+++ server.js
@@ -12,14 +12,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://rohanchatio-da8vgtug8-cs50x-rgb.vercel.app", // Change this to your client's origin
+    origin: [
+      "https://rohanchatio-da8vgtug8-cs50x-rgb.vercel.app",
+      "https://rohan-chat-io.firebaseapp.com"
+    ], // Change this to your client's origin
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
 });
 app.use(
   cors({
-    origin: "https://rohanchatio-da8vgtug8-cs50x-rgb.vercel.app", // Set the allowed origin
+    origin: [
+      "https://rohanchatio-da8vgtug8-cs50x-rgb.vercel.app",
+      "https://rohan-chat-io.firebaseapp.com"
+    ], // Change this to your client's origin // Set the allowed origin
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
