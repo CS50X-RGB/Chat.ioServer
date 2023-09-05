3ea3963291422f6d9bc8ad1e37353e71b87b2110
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-09-05 20:47:34 +0530

    Firebase url added

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chic-gumption-964552.netlify.app", // Change this to your client's origin
+    origin: "https://rohan-chat-io.web.app", // Change this to your client's origin
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
 });
 app.use(
   cors({
-    origin: "https://chic-gumption-964552.netlify.app", // Change this to your client's origin // Set the allowed origin
+    origin: "https://rohan-chat-io.web.app", // Change this to your client's origin // Set the allowed origin
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
