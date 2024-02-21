441c86ce7e631da53373895711fd0bf94b3e3609
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-02-22 01:13:15 +0530

    password validation in reset Password

--- Router/user.js
+++ Router/user.js
@@ -11,7 +11,7 @@ router.get("/logout", logout);
 router.get("/myProfile", isAuth, getMyProfile);
 router.get("/getUser/:name", isAuth, getMyId);
 router.get("/getUserData/:id", isAuth, getMyData)
-router.post("/forgotPassword", validatePassword, ForgetPassword);
-router.post('/resetpassword/:resetIdentifier', ResetPassword);
+router.post("/forgotPassword", ForgetPassword);
+router.post('/resetpassword/:resetIdentifier', validatePassword, ResetPassword);
 router.put("/updateDetails", isAuth, UpdateDetails);
 export default router;
