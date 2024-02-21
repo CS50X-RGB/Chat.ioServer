8bb304526355cf4309340c6c8eed6925d8a7dd56
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-02-22 01:12:31 +0530

    pass validation

--- Router/user.js
+++ Router/user.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { register, login, logout, getMyProfile, getMyId,getMyData, ForgetPassword, ResetPassword } from "../Controllers/user.js";
+import { register, login, logout, getMyProfile, getMyId, getMyData, ForgetPassword, ResetPassword } from "../Controllers/user.js";
 import { isAuth } from "../middleware/auth.js";
-import validateUserData from "../middleware/userValidation.js";
+import { validateUserData, validatePassword } from "../middleware/userValidation.js";
 
 const router = express.Router();
 
-router.post("/register",validateUserData, register);
+router.post("/register", validateUserData, register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/myProfile", isAuth, getMyProfile);
-router.get("/getUser/:name",isAuth,getMyId);
-router.get("/getUserData/:id",isAuth,getMyData)
-router.post("/forgotPassword",ForgetPassword);
+router.get("/getUser/:name", isAuth, getMyId);
+router.get("/getUserData/:id", isAuth, getMyData)
+router.post("/forgotPassword", validatePassword, ForgetPassword);
 router.post('/resetpassword/:resetIdentifier', ResetPassword);
-router.put("/updateDetails",isAuth,UpdateDetails);
+router.put("/updateDetails", isAuth, UpdateDetails);
 export default router;
--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -1,14 +1,37 @@
 import { z } from 'zod';
 
 const userSchema = z.object({
-        name: z.string().min(1),
+        name: z.string().min(1, { message: 'Name cant be empty' }),
         profilePic: z.string().min(1),
         user_token: z.string().optional(),
         email: z.string().email(),
-        password: z.string().min(6),
+        password: z.string().min(6, { message: 'Password should be at least 6 characters' }),
         refresh_user_token: z.string().optional(),
         createdAt: z.date().default(() => new Date())
 });
+
+const passwordSchema = z.object({
+        password: z.string().min(6, { message: 'Password should be at least 6 characters' })
+});
+
+export const validatePassword = (req, res, next) => {
+        const { password } = req.body;
+        try {
+                passwordSchema.parse({ password });
+                next();
+        } catch (error) {
+                const err = {
+                        path: 'password',
+                        message: error.errors[0].message
+                };
+                console.error(err);
+                return res.status(400).json({
+                        success: false,
+                        message: 'Invalid password',
+                        errors: [err]
+                });
+        };
+}
 
 export const validateUserData = (req, res, next) => {
         const userData = req.body;
@@ -28,8 +51,8 @@ export const validateUserData = (req, res, next) => {
                 res.status(400).json({
                         success: false,
                         message: 'Invalid user data',
-                        errors: err
+                        errors: err.map(error => error.message)
                 });
         }
 };
-export default validateUserData;
+export default { validateUserData, validatePassword };
