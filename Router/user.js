41d3eeb60edd3c5d347c0ba797d883273f42d634
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-02-24 00:14:17 +0530

    Removed the api end point of getting value on name

--- Router/user.js
+++ Router/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, logout, getMyProfile, getMyId, getMyData, ForgetPassword, ResetPassword,UpdateDetails } from "../Controllers/user.js";
+import { register, login, logout, getMyProfile, getMyData, ForgetPassword, ResetPassword,UpdateDetails } from "../Controllers/user.js";
 import { isAuth } from "../middleware/auth.js";
 import { validateUserData, validatePassword } from "../middleware/userValidation.js";
 
@@ -9,7 +9,6 @@ router.post("/register", validateUserData, register);
 router.post("/login", login);
 router.get("/logout",isAuth, logout);
 router.get("/myProfile", isAuth, getMyProfile);
-router.get("/getUser/:name", isAuth, getMyId);
 router.get("/getUserData/:id", isAuth, getMyData)
 router.post("/forgotPassword", ForgetPassword);
 router.post('/resetpassword/:resetIdentifier', validatePassword, ResetPassword);
