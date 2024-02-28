69533e3fff7c1c74de55dc035637279694eda651
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-02-28 22:15:26 +0530

    Deployed website

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://65df6141c9dd8236809a4509--cosmic-pegasus-9773bf.netlify.app",
+    origin: "https://luxury-kitten-419ac4.netlify.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
 });
 app.use(
   cors({
-    origin: "https://65df6141c9dd8236809a4509--cosmic-pegasus-9773bf.netlify.app",
+    origin: "https://luxury-kitten-419ac4.netlify.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
