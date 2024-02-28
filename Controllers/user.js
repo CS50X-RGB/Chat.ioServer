f0a527fff4e76e972a5e580a72c74168713f3e63
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-02-28 23:27:15 +0530

    changing Domains for reset passord

--- Controllers/user.js
+++ Controllers/user.js
@@ -192,7 +192,7 @@ export const ForgetPassword = async (req, res) => {
     const resetIdentifier = generateRandomToken();
     user.refresh_user_token = resetIdentifier;
     await user.save();
-    const resetLink = `https://luxury-kitten-419ac4.netlify.app/resetPassword/${resetIdentifier}`
+    const resetLink = `https://65df71d3ff26f9453f066f9e--effervescent-beignet-c0e563.netlify.app/resetPassword/${resetIdentifier}`
     sendEmail(user.email, `Click the following link to reset your password : ${resetLink}`);
     return res.status(200).json({
       success: true,
